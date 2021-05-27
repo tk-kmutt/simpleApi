90e087a2bdf779dacb72c19f9d3e8dac54988e83
kuwabara <takashi.k@cayto.jp>
2021-05-27 14:37:52 +0900

    validatorの追加

--- cmd/server.go
+++ cmd/server.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
 type User struct {
 	ID   string `json:"id"`
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+	Name string `json:"name" validate:"required"`
+	Age  int    `json:"age" validate:"max=150"`
 }
 
 type UserPatch struct {
@@ -19,12 +21,35 @@ type UserPatch struct {
 
 var users = make(map[string]*User)
 
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func NewValidator() *CustomValidator {
+	return &CustomValidator{
+		validator: validator.New(),
+	}
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	return nil
+}
+
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Validator = NewValidator()
 	e.POST("/simple", func(context echo.Context) error {
 		// リクエストを取得する
 		user := new(User)
 		_ = context.Bind(user)
+		// バリデーション
+		if err := context.Validate(user); err != nil {
+			return context.String(http.StatusBadRequest, err.Error())
+		}
 		users[user.ID] = user
 		return context.JSON(http.StatusCreated, user)
 	})
@@ -37,6 +62,10 @@ func main() {
 		// リクエストを取得する
 		id := context.Param("id")
 		user := new(User)
+		// バリデーション
+		if err := context.Validate(user); err != nil {
+			return context.JSON(http.StatusBadRequest, err)
+		}
 		_ = context.Bind(user)
 		users[id] = user
 		return context.JSON(http.StatusOK, user)
@@ -45,6 +74,10 @@ func main() {
 		// リクエストを取得する
 		id := context.Param("id")
 		user := new(UserPatch)
+		// バリデーション
+		if err := context.Validate(user); err != nil {
+			return context.JSON(http.StatusBadRequest, err)
+		}
 		_ = context.Bind(user)
 		base := users[id]
 		if user.Age != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module simpleApi
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.3.0
+require (
+	github.com/go-playground/validator/v10 v10.6.1
+	github.com/labstack/echo/v4 v4.3.0
+)
