8516d77c17c6f4d9f0d890e1d0e5a05cd3c0b75e
kuwabara <takashi.k@cayto.jp>
2021-05-28 15:03:39 +0900

    add mysql crud

--- cmd/server.go
+++ cmd/server.go
@@ -4,22 +4,24 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"net/http"
+	"simpleApi/internal/repository"
+	"time"
 )
 
 type User struct {
-	ID   string `json:"id"`
+	Code string `json:"code"`
 	Name string `json:"name" validate:"required"`
 	Age  int    `json:"age" validate:"max=150"`
 }
 
 type UserPatch struct {
-	ID   *string `json:"id"`
+	Code *string `json:"code"`
 	Name *string `json:"name"`
 	Age  *int    `json:"age"`
 }
-
-var users = make(map[string]*User)
 
 type CustomValidator struct {
 	validator *validator.Validate
@@ -42,6 +44,18 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Validator = NewValidator()
+
+	//mysql connection
+	dsn := "docker:docker@tcp(127.0.0.1:3306)/sampleApi?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+	// Migrate the schema
+	if err := db.AutoMigrate(&repository.Users{}); err != nil {
+		panic(err.Error())
+	}
+
 	e.POST("/simple", func(context echo.Context) error {
 		// リクエストを取得する
 		user := new(User)
@@ -50,49 +64,99 @@ func main() {
 		if err := context.Validate(user); err != nil {
 			return context.String(http.StatusBadRequest, err.Error())
 		}
-		users[user.ID] = user
+		// Create
+		now := time.Now()
+		db.Create(&repository.Users{
+			Code:      user.Code,
+			Name:      user.Name,
+			Age:       user.Age,
+			CreatedAt: now,
+			UpdatedAt: now,
+		})
 		return context.JSON(http.StatusCreated, user)
 	})
-	e.GET("/simple/:id", func(context echo.Context) error {
+	e.GET("/simple/:code", func(context echo.Context) error {
 		// リクエストを取得する
-		id := context.Param("id")
-		return context.JSON(http.StatusOK, users[id])
-	})
-	e.PUT("/simple/:id", func(context echo.Context) error {
-		// リクエストを取得する
-		id := context.Param("id")
-		user := new(User)
-		// バリデーション
-		if err := context.Validate(user); err != nil {
-			return context.JSON(http.StatusBadRequest, err)
+		code := context.Param("code")
+		m := new(repository.Users)
+		if tx := db.First(m, "code = ?", code); tx.Error != nil {
+			return context.String(http.StatusNotFound, tx.Error.Error())
 		}
-		_ = context.Bind(user)
-		users[id] = user
+
+		user := &User{
+			Code: m.Code,
+			Name: m.Name,
+			Age:  m.Age,
+		}
 		return context.JSON(http.StatusOK, user)
 	})
-	e.PATCH("/simple/:id", func(context echo.Context) error {
+	e.PUT("/simple/:code", func(context echo.Context) error {
 		// リクエストを取得する
-		id := context.Param("id")
+		code := context.Param("code")
+		user := new(User)
+		// バリデーション
+		_ = context.Bind(user)
+		if err := context.Validate(user); err != nil {
+			return context.JSON(http.StatusBadRequest, err)
+		}
+
+		m := new(repository.Users)
+		// First
+		if tx := db.First(m, "code = ?", code); tx.Error != nil {
+			return context.String(http.StatusNotFound, tx.Error.Error())
+		}
+		// Update
+		now := time.Now()
+		db.Model(m).
+			Where("code = ?", code).
+			Updates(repository.Users{
+				Name:      user.Name,
+				Age:       user.Age,
+				UpdatedAt: now,
+			})
+		return context.JSON(http.StatusOK, user)
+	})
+	e.PATCH("/simple/:code", func(context echo.Context) error {
+		// リクエストを取得する
+		code := context.Param("code")
 		user := new(UserPatch)
+		_ = context.Bind(user)
 		// バリデーション
 		if err := context.Validate(user); err != nil {
 			return context.JSON(http.StatusBadRequest, err)
 		}
-		_ = context.Bind(user)
-		base := users[id]
+
+		m := new(repository.Users)
+		// First
+		if tx := db.First(m, "code = ?", code); tx.Error != nil {
+			return context.String(http.StatusNotFound, tx.Error.Error())
+		}
+
+		tx := db.Model(m).Where("code = ?", code)
 		if user.Age != nil {
-			base.Age = *user.Age
+			m.Age = *user.Age
 		}
 		if user.Name != nil {
-			base.Name = *user.Name
+			m.Name = *user.Name
 		}
-		users[id] = base
-		return context.JSON(http.StatusOK, base)
+		tx.Updates(*m)
+		return context.JSON(http.StatusOK, &User{
+			Code: m.Code,
+			Name: m.Name,
+			Age:  m.Age,
+		})
 	})
-	e.DELETE("/simple/:id", func(context echo.Context) error {
+	e.DELETE("/simple/:code", func(context echo.Context) error {
 		// リクエストを取得する
-		id := context.Param("id")
-		users[id] = nil
+		code := context.Param("code")
+
+		m := new(repository.Users)
+		// First
+		if tx := db.First(m, "code = ?", code); tx.Error != nil {
+			return context.String(http.StatusNotFound, tx.Error.Error())
+		}
+		db.Delete(m, "code = ?", code)
+
 		return context.String(http.StatusNoContent, "")
 	})
 	e.Logger.Fatal(e.Start(":1232"))
